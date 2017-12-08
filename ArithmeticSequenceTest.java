8743d6b5db251d135368040126d775de7954b6c1
MartinKw92 <Kwapisiewicz.marcin@gmail.com>
2017-12-08 10:06:48 +0100

    Add files via upload

---
+++ ArithmeticSequenceTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArithmeticSequenceTest {
+
+    @Test
+    public void printArithmeticSequence() {
+        ArithmeticSequence arithmeticSequence = new ArithmeticSequence();
+        arithmeticSequence.printArithmeticSequence(4, 5, 5);
+    }
+}
